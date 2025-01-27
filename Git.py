dd95a291b121926a9c0d6647972d51259159b24e
Navin0305 <hereverything35@gmail.com>
2025-01-27 18:02:33 -0500

    Add Git.py program

---
+++ Git.py
@@ -0,0 +1,17 @@
+print("Navin, 100993272!")
+
+# Perform basic arithmetic
+num1 = 10
+num2 = 5
+
+# Addition
+print(f"Addition: {num1} + {num2} = {num1 + num2}")
+
+# Subtraction
+print(f"Subtraction: {num1} - {num2} = {num1 - num2}")
+
+# Multiplication
+print(f"Multiplication: {num1} * {num2} = {num1 * num2}")
+
+# Division
+print(f"Division: {num1} / {num2} = {num1 / num2}")
